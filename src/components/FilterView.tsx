d4eeb7bcc0162dd0c09023f63a0feb7ebad19ebb
emmadev <macanthonyemmanuel12@gmail.com>
2023-09-24 19:40:14 +0100

    implemented chip component

--- src/components/FilterView.tsx
+++ src/components/FilterView.tsx
@@ -32,7 +32,7 @@ const FilterView = () => {
       </View>
       {/** Range */}
       <View style={{ paddingHorizontal: 24 }}>
-        <View style={{ marginBottom: 15 }}>
+        <View style={{ marginBottom: 24 }}>
           <Text> Price Range </Text>
         </View>
         <View
@@ -47,11 +47,17 @@ const FilterView = () => {
             style={{
               position: "absolute",
               left: `${(100 * minPrice) / MAX_PRICE}%`,
-              width: `${(100 * (minPrice + maxPrice)) / MAX_PRICE}%`,
+              width: `${(100 * (maxPrice - minPrice)) / MAX_PRICE}%`,
               height: "100%",
               backgroundColor: theme.colors.primary,
             }}
-          ></View>
+          />
+          <View style={{ position: "absolute", left: "10%" }}>
+            <SliderHandle />
+          </View>
+          <View style={{ position: "absolute", left: "50%" }}>
+            <SliderHandle />
+          </View>
         </View>
         <View
           style={{
@@ -67,6 +73,94 @@ const FilterView = () => {
           </Text>
         </View>
       </View>
+      {/** Sports Category */}
+      <View style={{ paddingHorizontal: 24 }}>
+        <Text style={{ fontSize: 16, fontWeight: "600", marginBottom: 12 }}>
+          Sports
+        </Text>
+        <View style={{ flexDirection: "row", flexWrap: "wrap", gap: 12 }}>
+          {new Array(10).fill("").map((_, index) => {
+            return (
+              <Chip
+                key={index}
+                index={index}
+                label="Item"
+                isSelected={index === 0}
+              />
+            );
+          })}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const Chip = ({
+  index,
+  isSelected,
+  label,
+}: {
+  isSelected: boolean;
+  label: string;
+  index: number;
+}) => {
+  const theme = useTheme();
+
+  return (
+    <View
+      style={{
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderRadius: 100,
+        backgroundColor: isSelected
+          ? theme.colors.text
+          : theme.colors.background,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 14,
+          fontWeight: "600",
+          color: isSelected ? theme.colors.background : theme.colors.text,
+        }}
+      >
+        {label} {index}
+      </Text>
+    </View>
+  );
+};
+
+const SliderHandle = () => {
+  const theme = useTheme();
+  return (
+    <View
+      style={{
+        height: 24,
+        aspectRatio: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 100,
+        borderColor: theme.colors.primary,
+        borderWidth: 2,
+        transform: [
+          {
+            translateX: -12,
+          },
+          {
+            translateY: -12,
+          },
+        ],
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <View
+        style={{
+          width: 3,
+          height: 3,
+          borderRadius: 10,
+          backgroundColor: theme.colors.primary,
+        }}
+      />
     </View>
   );
 };
